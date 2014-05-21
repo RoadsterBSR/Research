937f382d1c6dc788690ab50aa0f52b9d7d06a3c3
Roel van Lisdonk <roelvanlisdonk@live.com>
2014-05-21 15:02:26 +0200

    Added tests

---
+++ Research/Research.EndToEndTests/CodeGeneration/SqlScriptGeneratorTester.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Research.EndToEndTests.CodeGeneration
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
+    using Research.Core.CodeGeneration;
+    using Research.Core.CodeGeneration.SqlScriptDtos;
+    using Research.Core.Components;
+    using Research.Core.Extensions;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class SqlScriptGeneratorTester
+    {
+        [TestMethod]
+        public void GetSqlScriptingInput_should_return_correct_input()
+        {
+            var generator = new SqlScriptGenerator();
+            SqlScriptingInput input = generator.GetSqlScriptingInput("MyServer", "MyDatabase");
+            input.RootFolder = @"C:\Temp\Generated";
+
+            SqlScriptingResult result = generator.GenerateSqlScripts(input);
+            var fs = new FileSystem();
+            Task deleteDirectoryTask = fs.DeleteDirectoryAsync(input.RootFolder);
+            deleteDirectoryTask.Wait();
+
+            Task generateFilesTask = fs.GenerateFilesAsync(result.Scripts);
+            generateFilesTask.Wait();
+
+            Assert.AreEqual(true, true);
+        }
+    }
+}
