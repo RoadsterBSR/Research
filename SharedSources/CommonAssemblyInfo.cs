da861c52cf1f1cfa212bbe78d9e72733a748c748
Roel van Lisdonk <roelvanlisdonk@live.com>
2014-06-05 08:17:19 +0200

    Added common assemblyinfo

---
+++ SharedSources/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Post-build script update beide versienummers naar TFS buildnumber format
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyCompanyAttribute("ADA ICT")]
+[assembly: AssemblyCopyrightAttribute("Copyright © ADA ICT 2014")]
