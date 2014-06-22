f1e885d0e9856c86605176d6e433180961d76c0f
roelvanlisdonk <roelvanlisdonk@live.com>
2014-06-22 22:16:35 +0200

    Added references

---
+++ Research/Research.UI.Web/Client/Features/Research/test.js
@@ -0,0 +1,37 @@
+﻿
+
+
+// var viewModel = kendo.observable(options);
+
+
+
+var routeMaker = routeMaker || {};
+routeMaker.features = routeMaker.features || {};
+routeMaker.features.beheer = routeMaker.features.beheer || {};
+routeMaker.features.beheer.ViewModel = {
+    
+    /// <field name='firstName' type='String'>The first name.</field>
+    firstName: "John",
+    lastName : "Doe",
+    fullName : function () {
+        return this.get("firstName") + " " + this.get("lastName");
+    }
+    
+    
+};
+
+routeMaker.features.beheer.controler = (function () {
+    var self = {};
+
+    
+    self.getViewModel = function () {
+        return new routeMaker.features.beheer.ViewModel();
+    };
+    
+    var _viewModel = self.getViewModel();
+       
+
+    return self;
+})();
+
+
