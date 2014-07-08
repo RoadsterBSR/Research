601db0db5648dbc1122cc8329fe7fe7abdc08edc
Roel van Lisdonk <roelvanlisdonk@live.com>
2014-07-08 16:50:55 +0200

    Worked on angular - breeze.

---
+++ Research/Research.UI.Web/Client/Features/Spa/app.js
@@ -0,0 +1,61 @@
+﻿
+// Module [app].
+(function ()
+{
+    'use strict';
+
+    var app = angular.module('app', [
+        // Angular modules 
+        'ngAnimate',        // animations
+        'ngRoute',          // routing
+        'ngSanitize'       // sanitizes html bindings (ex: sidebar.js)
+    ]);
+
+    // Handle routing errors and success events
+    app.run(['$route', function ($route)
+    {
+        // Include $route to kick start the router.
+    }]);
+})();
+
+// Controller [admin]
+(function ()
+{
+    'use strict';
+    var controllerId = 'admin';
+    angular.module('app').controller(controllerId, ['$scope', admin]);
+
+    function admin($scope)
+    {
+        var vm = this;
+        vm.title = 'Admin test page.';
+        vm.employees = [];
+
+        breeze.NamingConvention.camelCase.setAsDefault();
+        var manager = new breeze.EntityManager('/breeze/breeze');
+
+        var query = new breeze.EntityQuery()
+            .from("Employee");
+
+        manager.executeQuery(query).then(handleRefreshEmployees).fail(function (e)
+        {
+            alert(e);
+        });
+
+        function handleRefreshEmployees(data)
+        {
+            //vm.employees = [{ firstName: 'Test' }, { firstName: 'Test2' }];
+            vm.employees = data.results;
+            $scope.$apply();
+            //$scope.$apply(function ()
+            //{
+            //    refreshEmployees(data);
+            //});
+        }
+
+        function refreshEmployees(data)
+        {
+            vm.employees = [{ firstName: 'Test' }, { firstName: 'Test2' }];
+        }
+    }
+})();
