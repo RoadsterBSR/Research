602a4edd24bf374cf0c6a3e191b74d1327bb9cde
Roel van Lisdonk <roelvanlisdonk@live.com>
2014-09-03 17:08:56 +0200

    blog post

--- Research/Research.UI.Web/Client/Features/Vanilla/app.js
+++ Research/Research.UI.Web/Client/Features/Vanilla/app.js
@@ -5,30 +5,63 @@ spa.app = (function () {
     'use strict';
 
     var self = {};
+          
 
-    var model = {
-        label: 'Default',
-        completed: false
-    };
-    
-    function observer(changes)
+    self.Step1 = function ()
     {
-        changes.forEach(function (change, i)
+        self.Step2();
+    };
+
+    self.Step2 = function ()
+    {
+        self.Step3();
+    };
+
+    self.Step3 = function ()
+    {        
+        if (window.XMLHttpRequest)
         {
-            console.log('what property changed? ' + change.name);
-            console.log('how did it change? ' + change.type);
-            console.log('whats the current value? ' + change.object[change.name]);
-            console.log(change); // all changes
-        });
-    }
+            var request = new XMLHttpRequest();
+            request.open('GET', 'http://www.google.com', true);
+
+            request.onload = function ()
+            {
+                if (this.status >= 200 && this.status < 400)
+                {
+                    // Success!
+                    var data = JSON.parse(this.response);
+                    console.log(data);
+                } else
+                {
+                    // We reached our target server, but it returned an error
+                    console.log("Error status not between 200 and 400.");
+                }
+            };
+
+            request.onerror = function (e)
+            {
+                debugger;
+                // There was a connection error of some sort
+                console.log(e);
+            };
+
+            request.send();
+        }
+        
+    };
+
+    self.onExecuteClick = function ()
+    {
+        self.Step1();
+    };
 
     self.start = function ()
     {
-        // Start observing the model.
-        Object.observe(model, observer);
-
-        // Change model to show databinding.
-        model.label = 'Buy some more milk';
+        window.onerror = function (message, file, line, col, error)
+        {
+            debugger;
+            console.log(message, "from", error.stack);
+        };
     };
 
     return self;
