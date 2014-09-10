0f3a5a5245c5ca368023520f51339864cc20a594
Roel van Lisdonk <roelvanlisdonk@live.com>
2014-09-10 16:58:06 +0200

    Inlined javascript for lean development.

--- Research/Research.UI.Web/Client/Features/Vanilla/app.js
+++ Research/Research.UI.Web/Client/Features/Vanilla/app.js
@@ -5,67 +5,44 @@ spa.app = (function () {
     'use strict';
 
     var self = {};
-          
-
-    self.Step1 = function ()
-    {
-        self.Step2();
-    };
-
-    self.Step2 = function ()
-    {
-        self.Step3();
-    };
-
-    self.Step3 = function ()
-    {        
-        if (window.XMLHttpRequest)
-        {
-            var request = new XMLHttpRequest();
-            request.open('GET', 'http://www.google.com', true);
-
-            request.onload = function ()
-            {
-                if (this.status >= 200 && this.status < 400)
-                {
-                    // Success!
-                    var data = JSON.parse(this.response);
-                    console.log(data);
-                } else
-                {
-                    // We reached our target server, but it returned an error
-                    console.log("Error status not between 200 and 400.");
-                }
-            };
-
-            request.onerror = function (e)
-            {
-                debugger;
-                // There was a connection error of some sort
-                console.log(e);
-            };
-
-            request.send();
-        }
-        
-    };
-
-    self.onExecuteClick = function ()
-    {
-        self.Step1();
-    };
-
-    self.start = function ()
+    
+    self.initializeGlobalExceptionHandler = function ()
     {
         window.onerror = function (message, file, line, col, error)
         {
-            debugger;
             console.log(message, "from", error.stack);
         };
     };
 
+    self.start = function ()
+    {
+        spa.app.initializeGlobalExceptionHandler();
+
+        var offSet = getOffSetBasedOnCollapseStatus(300, 1);
+        adjustTop(document.getElementById('cmp-details'), offSet);
+    };
+
+    function adjustTop(element, value)
+    {
+        if (element && element.offsetTop && value)
+        {
+            element.style.top = (element.offsetTop + value) + 'px';
+        }
+    }
+
+    // Status: 0 = collapsed, 1 = expanded
+    function getOffSetBasedOnCollapseStatus(offSet, status)
+    {
+        var newOffSet = Math.abs(offSet);
+
+        if (offSet && status === 1)
+        {
+            newOffSet = -newOffSet;
+        }
+
+        return newOffSet;
+    }
+
     return self;
 })();
-
-// Start the application.
 spa.app.start();
