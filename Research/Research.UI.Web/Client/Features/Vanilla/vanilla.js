de882710addfe19ad72ce8c74f9b5dcfea7e10f7
Roel van Lisdonk <roelvanlisdonk@live.com>
2014-09-10 17:02:46 +0200

    Changed unit tests.

--- Research/Research.UI.Web/Client/Features/Vanilla/vanilla.js
+++ Research/Research.UI.Web/Client/Features/Vanilla/vanilla.js
@@ -1,10 +1,1 @@
 ﻿
-var vanilla  = (function () {
-    var self = {};
-
-    self.isNullOrUndefined = function (arg1) {
-        if (arg1 == null) { throw new Error("Variable [arg1] is null or undefined."); }
-    };
-
-    return self;
-})();
