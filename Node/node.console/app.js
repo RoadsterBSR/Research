87826da46d387d6df3a26fc1fb3ebcf043442ac8
Roel van Lisdonk <roelvanlisdonk@live.com>
2015-03-02 15:44:59 +0100

    Added research project for node

---
+++ Node/node.console/app.js
@@ -0,0 +1,27 @@
+﻿console.log('Hello world1');
+debugger;
+console.log('Hello world2');
+
+var del = require('del');
+
+console.log('Hello world 3');
+
+var Q = require('Q');
+
+function cleanFolder() {
+
+	var deferred = Q.defer()
+
+	del(['C:\Temp\temp2\**'], function (err, deletedFiles) {
+		console.log('Files deleted:', deletedFiles.join(', '));
+		deferred.resolve(deletedFiles);
+	});
+	// Fake ajax call
+	
+	
+	return deferred.promise;
+}
+
+cleanFolder().then(function (data) { 
+	console.log("Finished");
+});
