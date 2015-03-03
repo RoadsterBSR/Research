c7f3f93b7fb7567034a64e481a87a60807842cc5
roelvanlisdonk <roelvanlisdonk@live.com>
2015-03-03 22:02:48 +0100

    Just added some test code

--- Node/node.console/app.js
+++ Node/node.console/app.js
@@ -1,27 +1,51 @@
-﻿console.log('Hello world1');
-debugger;
-console.log('Hello world2');
+﻿// Start
 
-var del = require('del');
+console.log("Start application");
 
-console.log('Hello world 3');
 
-var Q = require('Q');
-
-function cleanFolder() {
-
-	var deferred = Q.defer()
-
-	del(['C:\Temp\temp2\**'], function (err, deletedFiles) {
-		console.log('Files deleted:', deletedFiles.join(', '));
-		deferred.resolve(deletedFiles);
-	});
-	// Fake ajax call
+(function (require) {
+	"use strict";
+			
+	// 'fs' is used to interact with the filesystem.
+	var fs = require('fs');
 	
+	function readFile() {
+		/// <summary>
+		/// Read the contents of the JSON schema file from filesystem.
+		/// </summary>
+		/// <returns type="Q">A promise.</returns>
 	
-	return deferred.promise;
-}
+		fs.readFile("store.schema.json", function (err, data) {
+			console.log("test");
+			if (err) {
+				console.log("err");
+				console.log(err);
+				throw err;
+			}
+			
+			
+			console.log("data");
+			console.log(typeof data);
+			var obj = JSON.parse(data);
+			console.log("after parse");
 
-cleanFolder().then(function (data) { 
-	console.log("Finished");
-});
+			console.log(obj);
+			
+			//var dataAsString = data.toJSON();
+			
+			//console.log("dataAsString");
+			//console.log(typeof dataAsString);
+			//console.log(dataAsString);
+			
+			////console.log(data.toString());
+			
+			//var object = JSON.parse(dataAsString);
+			//console.log(object);
+			
+
+			console.log("end read");
+		});
+	}
+
+	readFile();
+}(require));
