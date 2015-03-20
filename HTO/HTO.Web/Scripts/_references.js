7a85651509cd4f5518a2aa481a184019111feeb5
Roel van Lisdonk <roelvanlisdonk@live.com>
2015-03-20 15:51:17 +0100

    Added appcache file.

--- HTO/HTO.Web/Scripts/_references.js
+++ HTO/HTO.Web/Scripts/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="/Client/Core/Enums/enums.js" />
 /// <reference path="/Client/Core/Settings/settings.js" />
 /// <reference path="/Client/Core/Services/global.error.handling.js" />
+/// <reference path="/Client/Core/Services/appcache.js" />
 /// <reference path="/Client/Core/Services/geolocation.js" />
 /// <reference path="/Client/Core/Services/canvas.js" />
 /// <reference path="/Client/Core/Models/app.js" />
--- HTO/HTO.Web/gulpfile.js
+++ HTO/HTO.Web/gulpfile.js
@@ -1,40 +1,133 @@
 ﻿
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var plumber = require('gulp-plumber');
-var livereload = require('gulp-livereload');
+(function (require) {
+    "use strict";
 
-// Gulp plumber error handler
-var onError = function (err) {
-	console.log(err);
-};
+    // "through2" is a thin wrapper around node transform streams.
+    var through = require("through2");
 
-gulp.task('reload', function () {
-	// Change the filepath, when you want to live reload a different page in your project.
-	livereload.reload("./index.html");
-});
+    // "fs" is used to interact with the filesystem.
+    var fs = require("fs");
 
-// This task should be run, when you want to reload the webpage, when files change on disk.
-// This task will only watch JavaScript file changes in the folder "/Client" and it's subfolders.
-gulp.task('watch', function () {
-	livereload.listen();
-	gulp.watch('./Client/**/*.js', ['reload']);
-});
+    // "Q" is used to work with promises instead of callbacks.
+    var Q = require("Q");
 
-// Hint all of our custom developed Javascript to make sure things are clean.
-// This task will only hint JavaScript files in the folder "/Client" and it's subfolders.
-gulp.task('jshint', function () {
-	return gulp.src([
-        './Client/**/*.js'
-	])
-	.pipe(plumber({
-		errorHandler: onError
-	}))
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'));
-});
+    // Gulp.
+    var gulp = require("gulp");
 
-// When the user enters "gulp" on the command line, the default task will automatically be called.
-// This default task below, will run all other task automatically.
-// So when the user enters "gulp" on the command line all task are run.
-gulp.task('default', ['jshint']);
+    // Gulp plugins.
+    var jshint = require("gulp-jshint");
+    var plumber = require("gulp-plumber");
+    var livereload = require("gulp-livereload");
+
+    function fingerPrintAppCache(content, datetime) {
+        /// <summary>
+        /// Replaces the datetime in appcache file with the given datetime.
+        /// The "appcache" file should contain the text: "# version {{ ... }}" or the text: "# version {{...}}"
+        /// Example of the finger printed row:
+        /// 
+        /// # version {{ 2015-03-20T13:50:18.808Z }}
+        ///
+        /// </summary>
+        /// <param name="content" type="String">The content of the ".appcache" file.</param>
+        /// <param name="datetime" type="Date">Current date time</param>
+
+        var regEx = new RegExp("# version {{.*}}", "g");
+        var result = content.replace(regEx, "# version {{ " + datetime.toISOString() + " }}");
+        return result;
+    }
+
+    // Gulp plumber error handler
+    var onError = function (err) {
+        console.log(err);
+    };
+
+    function readFile(path) {
+        /// <summary>
+        /// Read the contents of the given file.
+        /// It returns a promise.
+        /// </summary>
+        /// <returns type="Q">A promise.</returns>
+
+        var deferred = Q.defer();
+        fs.readFile(path, "utf-8", function (err, data) {
+            if (err) {
+                deferred.reject(err);
+            }
+            deferred.resolve(data);
+        });
+
+        return deferred.promise;
+    }
+
+    function reload() {
+        /// <summary>
+        /// Change the filepath, when you want to live reload a different page in your project.
+        /// </summary>
+
+        livereload.reload("./index.html");
+    }
+
+    function writeFile(path, content) {
+        /// <summary>
+        /// Write one model to filesystem.
+        /// </summary>
+        /// <param name="path" type="String">Full file path.</returns>
+        /// <param name="content" type="String">Content to be written to file.</returns>
+
+        var ws = fs.createWriteStream(path);
+        ws.write(content);
+        ws.end();
+    }
+
+    gulp.task("fingerprint-appcache", function () {
+        /// <summary>
+        /// Fingerprint the "manifest.appcache" file, so it is invalidated.
+        /// </summary>
+
+        var path = "./Resources/manifest.appcache";
+        readFile(path)
+            .then(function (content) {
+                content = fingerPrintAppCache(content, new Date());
+                writeFile(path, content);
+                reload();
+            })
+            .done();
+    });
+
+    gulp.task("reload", reload);
+
+    gulp.task("watch", function () {
+        /// <summary>
+        /// This task should be run, when you want to reload the webpage, when files change on disk.
+        /// This task will only watch JavaScript file changes in the folder "/Client" and it's subfolders.
+        /// It will start by finger printing the "appcache" file, so al files will be refreshed in the browser.
+        /// </summary>
+
+        livereload.listen();
+        gulp.watch("./Client/**/*.js", ["fingerprint-appcache"]);
+    });
+
+    
+    gulp.task("jshint", function () {
+        /// <summary>
+        /// Hint all of our custom developed Javascript to make sure things are clean.
+        /// This task will only hint JavaScript files in the folder "/Client" and it's subfolders.
+        /// </summary>
+
+        return gulp.src([
+            "./Client/**/*.js"
+        ])
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"));
+    });
+
+    // When the user enters "gulp" on the command line, the default task will automatically be called.
+    // This default task below, will run all other task automatically.
+    // So when the user enters "gulp" on the command line all task are run.
+    gulp.task("default", ["jshint"]);
+    
+}(require));
+
